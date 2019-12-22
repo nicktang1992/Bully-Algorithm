fe42a9f0a09f9e5341040601e5e6638b6f242b19
Zhengyang Tang <ztang@terpmail.umd.edu>
2019-12-22 03:01:00 -0500

    modifying invalid argument exceptions

--- src/bully/IncorrectArgumentsException.java
+++ src/bully/IncorrectArgumentsException.java
@@ -4,10 +4,10 @@ public class IncorrectArgumentsException extends Exception {
 	private static final long serialVersionUID = 3582791246080108540L;
 
 	public IncorrectArgumentsException() {
-		super("Incorrect arguments.\nUsage: java -jar bully.jar uuid port nodeType timeout configuration-file [Initiator]");
+		super("Incorrect arguments.\nUsage: java -jar Bully.jar ip configuration-file [NoInitialization]");
 	}
 	
 	public IncorrectArgumentsException(String message) {
-		super(message + "\nUsage: java -jar bully.jar uuid port nodeType timeout configuration-file [Initiator]");
+		super(message + "\nUsage: java -jar Bully.jar ip configuration-file [NoInitialization]");
 	}
 }
