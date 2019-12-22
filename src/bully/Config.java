7e1a0819561ff9d6ccaba8f0053f68fd7a141d68
Zhengyang Tang <ztang@terpmail.umd.edu>
2019-12-22 18:24:08 -0500

    Read local IP adress

--- src/bully/Config.java
+++ src/bully/Config.java
@@ -1,7 +1,9 @@
 package bully;
+import java.net.InetAddress;
 
 public class Config {
 	public static int CONNECTION_TIMEOUT = 3000;
 	public static int HEARTBEAT_INTERVAL = 5000;
 	public static int PORT = 9000;
+	public static String CONFIG_FILE_NAME = "config.txt";
 }
